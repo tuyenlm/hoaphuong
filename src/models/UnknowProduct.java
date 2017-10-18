e5934e47bf01d401ae605e461d196d5d7eb4dc38
tuyenlm <tuyenlm1986@gmail.com>
2017-10-19 02:36:17 +0900

    sekect item

---
+++ src/models/UnknowProduct.java
@@ -0,0 +1,23 @@
+package models;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class UnknowProduct {
+	private final SimpleStringProperty barcodeUnknow;
+
+	public UnknowProduct(String barcodeUnknow) {
+		this.barcodeUnknow = new SimpleStringProperty(barcodeUnknow);
+	}
+
+	public SimpleStringProperty barcodeUnknowProperty() {
+		return this.barcodeUnknow;
+	}
+
+	public String getBarcodeUnknow() {
+		return this.barcodeUnknowProperty().get();
+	}
+
+	public void setBarcodeUnknow(final String barcodeUnknow) {
+		this.barcodeUnknowProperty().set(barcodeUnknow);
+	}
+}
