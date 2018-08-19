056492180eb86588e45589aa40fbe04149944b90
tuyenlm <tuyenlm1986@gmail.com>
2018-08-19 18:38:20 +0900

    sss

--- src/database/DbHandler.java
+++ src/database/DbHandler.java
@@ -12,7 +12,7 @@ public class DbHandler {
 		String port = "5432";
 		String dbname = "hoaphuong";
 		String user = "postgres";
-		String password = "postgres";
+		String password = "abc123";
 		final String ConnectionString = "jdbc:postgresql://" + host + ":" + port + "/" + dbname;
 
 		try {
